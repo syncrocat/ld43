5a26bb3ff2bc87eceb2efd2d9a87c3a9c27ed69f
syncrocat <r.grant.zeidler@gmail.com>
2018-12-02 13:58:22 -0500

    Pushed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-app.renderer = PIXI.autoDetectRenderer(725, 720);
+app.renderer = PIXI.autoDetectRenderer(720, 720);
 app.renderer.backgroundColor = 0xff0000;
 
 // Add the canvas to the HTML document
@@ -7,7 +7,7 @@ document.body.appendChild(app.renderer.view);
 app.stage = new PIXI.Container();
 app.stage.interactive = true;
 app.stage.buttonMode = true;
-app.stage.hitArea = new PIXI.Rectangle(0, 0, 725, 720);
+app.stage.hitArea = new PIXI.Rectangle(0, 0, 720, 720);
 app.stage.on('mousedown', app.mouseDown)
 app.stage.on('mouseup', app.mouseUp)
 
@@ -17,17 +17,33 @@ PIXI.loader
 .add("pics/tempcardselected.png")
 .add("pics/wolfcard.png")
 .add("pics/deercard.png")
-.add("pics/tempdeckcard.png")
-.add("pics/tempsubmit.png")
+.add("pics/deck.png")
+.add("pics/commitbutton.png")
+.add("pics/helpbutton.png")
+.add("pics/treeswampwater.png")
+.add("pics/treetreewater.png")
+.add("pics/treewaterwater.png")
+.add("pics/treeswampoil.png")
+.add("pics/treetreeoil.png")
+.add("pics/treeoiloil.png")
+.add("pics/background.png")
+.add("pics/cardbackground.png")
+.add("pics/logbackground.png")
+.add("pics/popup.png")
+.add("pics/cloudbackground.png")
 .load(function () {
     app.setup();
 });
 
 app.setup = function () {
     app.mouse_pressed = false;
+    let background = new graphics.BackgroundObj();
+    background.init();
 
     let gameBoard = new GameBoard();
     gameBoard.init();
+    let deckObj = new graphics.Deck();
+    deckObj.init();
     let deck = new Deck()
     deck.init(gameBoard, 25)
     let saveDeck = new Deck()
@@ -36,14 +52,27 @@ app.setup = function () {
     let submitButton = new graphics.SubmitObj();
     hand.init(gameBoard, deck, saveDeck, submitButton)
     submitButton.init(hand);
+    let world = new graphics.World();
+    world.init();
+    let log = new graphics.Log();
+    log.init();
+    let cardBackgrounds = [];
+    for (let i = 0; i < 3; i++) {
+        cardBackgrounds.push(new graphics.CardBackground());
+        cardBackgrounds[i].init(15 + (202 + 42) * i, 500);
+    }
+    let helpButton = new graphics.HelpButtonObj();
+    helpButton.init();
+
+    
     
     hand.drawCards();
 
     let gameBox = {
         hand:hand,
         submit:submitButton,
-        gameBoard:gameBoard,
-    }
+        gameBoard:gameBoard
+    };
     // Start the game loop
     app.gameLoop(gameBox);
 }
